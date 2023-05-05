31f49f908e94c63ccc069b042c8b46185991228b
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-05-05 15:11:39 +0100

    get tensor ancestors

--- minigrad/tensor.py
+++ minigrad/tensor.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 from numpy.typing import NDArray
-import numpy as np
-from numpy import zeros_like, ones_like
-from typing import Optional, Generic, Iterator, Generator
+from typing import Optional, Generator, Iterable
 from enum import StrEnum
+from collections import deque
 
 class Op(StrEnum):
   add = '+'
@@ -14,28 +13,27 @@ class BadTensor:
   data: NDArray
   # partial derivative
   grad: Optional[NDArray] = None
-  parent: Optional[BadTensor]
+  parents: Iterable[BadTensor]
   op: Optional[Op]
   label: Optional[str]
   def __init__(
     self,
     data: NDArray,
-    parent: Optional[BadTensor] = None,
+    parents: Iterable[BadTensor] = (),
     op: Optional[Op] = None,
     label: Optional[str] = None
   ) -> None:
     self.data = data
-    self.parent = parent
+    self.parents = parents
     self.op = op
     self.label = label
 
   # https://www.programiz.com/python-programming/operator-overloading
   def __add__(self, other: BadTensor) -> BadTensor:
-    dother_dself = ones_like(other.data)
-    return BadTensor(self.data + other.data, parent=self, op=Op.add)
+    return BadTensor(self.data + other.data, parents=(self, other), op=Op.add)
 
   def __mul__(self, other: BadTensor) -> BadTensor:
-    return BadTensor(self.data * other.data, parent=self, op=Op.mul)
+    return BadTensor(self.data * other.data, parents=(self, other), op=Op.mul)
   
   def label_(self, label: str) -> BadTensor:
     self.label = label
@@ -47,6 +45,8 @@ class BadTensor:
     return f'{label_prefix}{data}'
   
   def ancestors(self) -> Generator[BadTensor, None, None]:
-    if self.parent is not None:
-      yield self.parent
-      yield from self.parent.ancestors()
+    to_visit: deque[BadTensor] = deque(self.parents)
+    while to_visit:
+      tensor: BadTensor = to_visit.popleft()
+      yield tensor
+      to_visit.extend(tensor.parents)
