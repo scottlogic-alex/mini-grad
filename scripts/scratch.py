0dca1d812422692c0a4e96db1c391edc23815a16
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-05-12 09:47:44 +0100

    learns to convert feet-inches to m, cm, mm, given batch-of-1 inputs.

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -8,28 +8,35 @@ from itertools import islice
 
 set_printoptions(suppress=True)
 def feet_inches_mat() -> Generator[BadTensor, None, None]:
-  # inputs = rand(16, 2) * r_[10, 12] # array([[6., 0.], [5., 2.], [4., 5.], [3., 11.], [2., 7.]])
-  # inputs = rand(1, 2) * r_[10, 12]
-  inputs = rand(1, 1) * r_[10]
-  # true_weights = array([[2.54*12*.01, 2.54*.01], [2.54*12, 2.54], [2.54*12*10, 2.54*10]]).T
+  minibatch_size = 1
+  true_weights = array([[2.54*12*.01, 2.54*.01], [2.54*12, 2.54], [2.54*12*10, 2.54*10]]).T
+  # true_weights = array([[2.54*12, 2.54], [2.54*12*10, 2.54*10]]).T
   # true_weights = array([[2.54*12, 2.54]]).T
-  true_weights = array([[2.54*12]]).T
-  minibatch_size = inputs.shape[0]
-  lr = 5e-3# * minibatch_size
-  feet_inches = BadTensor(inputs, label='input_feet_inches')
+  # true_weights = array([[2.54*12]]).T
+  # lr = 5e-3# * minibatch_size
+  lr = 1e-2# * minibatch_size
   # should converge on true_weights
-  # feet_inches_to_cm = BadTensor(array([[0., 0.], [0., 0.], [0., 0.]]).T, label='learned_feet_inches_to_m_cm_mm', train_me=True)
+  feet_inches_to_cm = BadTensor(array([[0., 0.], [0., 0.], [0., 0.]]).T, label='learned_feet_inches_to_m_cm_mm', train_me=True)
+  # feet_inches_to_cm = BadTensor(array([[0., 0.], [0., 0.]]).T, label='learned_feet_inches_to_cm_mm', train_me=True)
   # feet_inches_to_cm = BadTensor(array([[0., 0.]]).T, label='learned_feet_inches_to_cm', train_me=True)
-  feet_inches_to_cm = BadTensor(array([[0.]]).T, label='learned_feet_to_cm', train_me=True)
+  # feet_inches_to_cm = BadTensor(array([[0.]]).T, label='learned_feet_to_cm', train_me=True)
   # feet_inches_to_cm = BadTensor(true_weights, label='learned_feet_inches_to_m_cm_mm', train_me=True)
-  expected_cm = BadTensor(inputs @ true_weights, label='true_cm_mm')
 
   while True:
-    predicted_cm = (feet_inches @ feet_inches_to_cm).label_('predicted_cm')
+    # inputs = rand(16, 2) * r_[10, 12] # array([[6., 0.], [5., 2.], [4., 5.], [3., 11.], [2., 7.]])
+    inputs = rand(minibatch_size, 2) * r_[10, 12]
+    input_feet_inches = BadTensor(inputs, label='input_feet_inches')
+    expected_cm = BadTensor(inputs @ true_weights, label='true_cm_mm')
+
+    # inputs = rand(1, 1) * r_[10]
+    predicted_cm = (input_feet_inches @ feet_inches_to_cm).label_('predicted_cm')
     diff = (expected_cm - predicted_cm).label_('diff')
     L2 = (diff ** 2).label_('L2')
     sum = (L2.sum()).label_('sum')
     mse = (sum / L2.data.size).label_('MSE')
+
+    if mse.data[0] < 1:
+      pass
 
     mse.backward()
 
@@ -71,10 +78,12 @@ def simple_sum() -> Generator[BadTensor, None, None]:
       t.grad = zeros_like(t.data)
 
 # it, needle_label = simple_sum(), 'learned_bias'
-it, needle_label = feet_inches_mat(), 'learned_feet_to_cm'
+# it, needle_label = feet_inches_mat(), 'learned_feet_inches_to_cm'
+# it, needle_label = feet_inches_mat(), 'learned_feet_inches_to_cm_mm'
+it, needle_label = feet_inches_mat(), 'learned_feet_inches_to_m_cm_mm'
 
 for step, _ in zip(it, range(100)):
   print(f'loss: {step.data[0]}')
 needle: BadTensor = next((t for t in step.ancestors() if t.label == needle_label))
-print(needle)
+print(needle.data)
 pass # somewhere to put a breakpoint
